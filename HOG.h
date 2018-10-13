099e693e6f9eea6a11ff59b83536bdd678e0706a
kursat <kursat.petek@hotmail.de>
2018-10-13 14:17:59 +0200

    New file .gitignore added to ignore subdirectories and files which are not relevant.

--- HOG.h
+++ HOG.h
@@ -1,9 +1,17 @@
 #pragma once
 
-#include <opencv2\core\core.hpp>
-#include <opencv2\imgproc\imgproc.hpp>
-#include <opencv2\highgui\highgui.hpp>
-#include <opencv2\ml\ml.hpp>
+// OpenCv includes
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
 
 #include <iostream>
 #include <fstream>
